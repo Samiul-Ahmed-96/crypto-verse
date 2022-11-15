903b40665c9b93b2ebd212d10713a49ae27a68be
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-11-15 15:01:38 +0600

    Load Data Dynamically

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Card, Col, Row, Typography } from "antd";
+import millify from "millify";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+const { Title } = Typography;
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+
+  console.log(cryptos);
+
+  if(isFetching) return 'Loading';
+
   return (
-    <div>Cryptocurrencies</div>
-  )
-}
+    <>
+      
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+            {/* Note: Change currency.id to currency.uuid  */}
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    width={"25px"}
+                    className="crypto-image"
+                    src={currency.iconUrl}
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {currency.change}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,14 @@
 import { Col, Row, Statistic, Typography } from "antd";
+import millify from "millify";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Cryptocurrencies, News } from "../components";
 import { useGetCryptosQuery } from "../services/cryptoApi";
-
 const { Title } = Typography;
 
 const Homepage = () => {
 
-  const {data , isFetching} = useGetCryptosQuery();
+  const {data , isFetching} = useGetCryptosQuery(10);
 
   const totalCryptocurrencies = data?.data.stats.total;
   const totalExchanges = data?.data.stats.totalExchanges;
@@ -22,25 +24,39 @@ const Homepage = () => {
       <Title level={2}>Global Crypto Stats</Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={totalCryptocurrencies}></Statistic>
+          <Statistic  title="Total Cryptocurrencies" value={totalCryptocurrencies}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={totalExchanges}></Statistic>
+          <Statistic title="Total Exchanges" value={millify(totalExchanges)}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={totalMarketCap}></Statistic>
+          <Statistic title="Total Market Cap" value={millify(totalMarketCap)}></Statistic>
         </Col>
 
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={total24hVolume}></Statistic>
+          <Statistic title="Total 24h Volume" value={millify(total24hVolume)}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={totalMarkets}></Statistic>
+          <Statistic title="Total Markets" value={millify(totalMarkets)}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Coins" value={totalCoins}></Statistic>
+          <Statistic title="Total Coins" value={millify(totalCoins)}></Statistic>
         </Col>
       </Row>
+      <div className="cryptocurrencies-container">
+          <div className="cryptocurrencies-heading">
+            <Title level={2}>Top 10 Crypto Currencies In The World</Title>
+            <Title level={4}><Link to="/cryptocurrencies">Show More</Link></Title>
+          </div>
+          <Cryptocurrencies simplified/>
+      </div>
+      <div className="news-container">
+          <div className="news-heading">
+            <Title level={2}>Latest Crypto News</Title>
+            <Title level={4}><Link to="/news">Show More</Link></Title>
+          </div>
+          <News simplified/>
+      </div>
     </>
   );
 };
