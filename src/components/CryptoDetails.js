0f03ec61758aae4b64a1838160bfca81d3d305fb
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-11-25 18:01:26 +0600

    Fetch Details

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,29 +1,31 @@
-import { Col, Typography } from 'antd';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { Col, Typography } from "antd";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
 
-const {Title , Text} = Typography;
+const { Title, Text } = Typography;
 
 const CryptoDetails = () => {
-  const [timePeriod,setTimePeriod]=useState('7d')
-  const {coinId} = useParams();
-  const {data , isFetching} = useGetCryptoDetailsQuery(coinId);
+  const [timePeriod, setTimePeriod] = useState("7d");
+  const { coinId } = useParams();
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
-
 
   console.log(cryptoDetails);
 
   return (
-    <Col className='coin-detail-container'>
-      <Col className='coin-heading-container'>
+    <div className="crypto-details-main">
+      <Col className="coin-heading-container">
         <Title level={2} className="coin-name">
-          {cryptoDetails?.name}  Price : {cryptoDetails?.price}
+          {data?.data?.coin.name} ({data?.data?.coin.symbol}) Price
         </Title>
-        <p>{cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.</p>
+        <p>
+          {cryptoDetails.name} live price in US Dollar (USD). View value
+          statistics, market cap and supply.
+        </p>
       </Col>
-    </Col>
-  )
-}
+    </div>
+  );
+};
 
-export default CryptoDetails
+export default CryptoDetails;
